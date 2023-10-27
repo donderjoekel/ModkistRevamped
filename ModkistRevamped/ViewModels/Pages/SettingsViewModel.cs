3c564a6977daa0cd4d72a22a9944d72dd8377d09
Christiaan Bloemendaal <github@tnrd.net>
2023-10-27 19:46:09 +0200

    Initial project

---
+++ ModkistRevamped/ViewModels/Pages/SettingsViewModel.cs
@@ -0,0 +1,66 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wpf.Ui.Controls;
+
+namespace TNRD.Modkist.ViewModels.Pages;
+
+public partial class SettingsViewModel : ObservableObject, INavigationAware
+{
+    private bool _isInitialized = false;
+
+    [ObservableProperty] private string _appVersion = string.Empty;
+
+    [ObservableProperty] private Wpf.Ui.Appearance.ThemeType _currentTheme = Wpf.Ui.Appearance.ThemeType.Unknown;
+
+    public void OnNavigatedTo()
+    {
+        if (!_isInitialized)
+            InitializeViewModel();
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+
+    private void InitializeViewModel()
+    {
+        CurrentTheme = Wpf.Ui.Appearance.Theme.GetAppTheme();
+        AppVersion = $"ModkistRevamped - {GetAssemblyVersion()}";
+
+        _isInitialized = true;
+    }
+
+    private string GetAssemblyVersion()
+    {
+        return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+               ?? string.Empty;
+    }
+
+    [RelayCommand]
+    private void OnChangeTheme(string parameter)
+    {
+        switch (parameter)
+        {
+            case "theme_light":
+                if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Light)
+                    break;
+
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Light);
+                CurrentTheme = Wpf.Ui.Appearance.ThemeType.Light;
+
+                break;
+
+            default:
+                if (CurrentTheme == Wpf.Ui.Appearance.ThemeType.Dark)
+                    break;
+
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Dark);
+                CurrentTheme = Wpf.Ui.Appearance.ThemeType.Dark;
+
+                break;
+        }
+    }
+}
