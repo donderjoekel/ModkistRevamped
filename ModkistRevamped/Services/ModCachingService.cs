92f4f53fc4df4a7ceba59621d288ff1b2aeccf36
Christiaan <github@tnrd.net>
2026-02-10 20:04:16 +0100

    fix: mod caching service when user is not signed in

--- ModkistRevamped/Services/ModCachingService.cs
+++ ModkistRevamped/Services/ModCachingService.cs
@@ -9,21 +9,36 @@ public class ModCachingService : IEnumerable<Mod>
 {
     private readonly ModsClient modsClient;
     private readonly UserClient userClient;
+    private readonly SettingsService settingsService;
     private readonly Dictionary<uint, Mod> idToMod = new();
 
-    public ModCachingService(ModsClient modsClient, UserClient userClient)
+    public ModCachingService(ModsClient modsClient, UserClient userClient, SettingsService settingsService)
     {
         this.modsClient = modsClient;
         this.userClient = userClient;
+        this.settingsService = settingsService;
     }
 
     public async Task Initialize()
     {
         idToMod.Clear();
 
-        var results = await Task.WhenAll(modsClient.Search().ToList(), userClient.GetSubscriptions().ToList(), userClient.GetMods().ToList());
+        List<Task<IReadOnlyList<Mod>>> tasks = new()
+        {
+            modsClient.Search().ToList()
+        };
+        
+        if (settingsService.AccessToken != null)
+        {
+            tasks.Add(userClient.GetSubscriptions().ToList());
+            tasks.Add(userClient.GetMods().ToList());
+        }
+
+        var results = await Task.WhenAll(tasks);
+
         IReadOnlyList<Mod> mods = results.SelectMany(list => list)
                                         .DistinctBy(mod => mod.Id)
+                                        .Where(x => x.GameId == 3213)
                                         .ToList();
 
         foreach (Mod mod in mods)
